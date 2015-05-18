5f649caca451605b36a118305366551126992aa1
ThanasiStratigakis <thanasi.strati@gmail.com>
2015-05-18 19:50:49 -0400

    Dispatch Activity opens either the main activity or the sign up/ login activity

--- mobile/src/main/java/com/example/thanasistratigakis/servicelog/DispatchActivity.java
+++ mobile/src/main/java/com/example/thanasistratigakis/servicelog/DispatchActivity.java
@@ -1,7 +1,27 @@
 package com.example.thanasistratigakis.servicelog;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.parse.ParseUser;
+
 /**
  * Created by ThanasiStratigakis on 5/18/15.
  */
-public class DispatchActivity {
+public class DispatchActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        // Check if there is  current user info
+        if (ParseUser.getCurrentUser() != null) {
+            // Start an intent for th elogged in activity
+            startActivity(new Intent(this, MainActivity.class));
+        } else {
+            // Start and intent for the logged out activity
+            startActivity(new Intent(this, SignUpOrLoginActivity.class));
+        }
+    }
+
 }
