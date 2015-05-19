91e3a5a1485ed34f44bfd0cd87b1bff4c4239229
ThanasiStratigakis <thanasi.strati@gmail.com>
2015-05-19 00:26:47 -0400

    Login code implemented but crashes

--- mobile/src/main/java/com/example/thanasistratigakis/servicelog/LoginActivity.java
+++ mobile/src/main/java/com/example/thanasistratigakis/servicelog/LoginActivity.java
@@ -1,7 +1,95 @@
 package com.example.thanasistratigakis.servicelog;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
 /**
  * Created by ThanasiStratigakis on 5/18/15.
  */
-public class LoginActivity {
+public class LoginActivity extends Activity {
+
+    private EditText usernameView;
+    private EditText passwordView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        usernameView = (EditText) findViewById(R.id.txtUsername);
+        passwordView = (EditText) findViewById(R.id.txtPassword);
+
+        // Set up the login button click handler
+        findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Validate the login data
+                boolean validationError = false;
+                StringBuilder validationErrorMessage = new StringBuilder("Please ");
+                if(isEmpty(usernameView)){
+                    validationError = true;
+                    validationErrorMessage.append("enter a username");
+                }
+                if (isEmpty(passwordView)) {
+                    if (validationError) {
+                        validationErrorMessage.append(", and ");
+                    }
+                    validationError = true;
+                    validationErrorMessage.append("enter a password");
+                }
+                validationErrorMessage.append(".");
+
+                // If there is a validation error, display the error
+                if (validationError) {
+                    Toast.makeText(LoginActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                // Set up a progress dialog
+                final ProgressDialog dlg = new ProgressDialog(LoginActivity.this);
+                dlg.setTitle("please wait.");
+                dlg.setMessage("Logging in.  Please wait.");
+                dlg.show();
+                // Call the Parse login method
+                ParseUser.logInInBackground(usernameView.getText().toString(), passwordView.getText().toString(), new LogInCallback() {
+                    @Override
+                    public void done(ParseUser parseUser, ParseException e) {
+                        dlg.dismiss();
+                        if (e != null) {
+                            // Show the error message
+                            Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                        } else {
+                            // Start an intent for the dispatch activity
+                            Intent intent = new Intent(LoginActivity.this, DispatchActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
+            }
+        });
+
+    }
+
+
+    // checks if the text is > 0
+    private boolean isEmpty (EditText etText) {
+        // gets text, makes it a string, takes out spaces, gets length
+        if (etText.getText().toString().trim().length() > 0) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
--- mobile/src/main/java/com/example/thanasistratigakis/servicelog/SampleApplication.java
+++ mobile/src/main/java/com/example/thanasistratigakis/servicelog/SampleApplication.java
@@ -2,6 +2,8 @@ package com.example.thanasistratigakis.servicelog;
 
 import android.app.Application;
 
+import com.parse.Parse;
+
 /**
  * Created by ThanasiStratigakis on 5/18/15.
  */
@@ -9,5 +11,11 @@ public class SampleApplication extends Application{
 
     public void onCreate() {
         //Parse.initialize(this, getString(R.string.parse_app_id), getString(R.string.parse_client_id));
+
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "bFlfAKQNEWDRvaNX7ikcJ0ZaSKbcKc6W98InH9Ie", "Idlc9KjBgLFvLakk2jUMBpUEfvUNZemux6tT10p9");
     }
 }
